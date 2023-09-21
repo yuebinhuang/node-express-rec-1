2d94ffa6eeff655a3464dfbe96bc79b72bd59acc
Yuebin <yuebin25@mit.edu>
2023-09-21 03:02:46 -0400

    fixed bug

--- src/concepts/websession.ts
+++ src/concepts/websession.ts
@@ -28,6 +28,9 @@ export default class WebSessionConcept {
     // Hint: Take a look at how the "end" function makes sure the user is logged in. Keep in mind that a
     // synchronization like starting a session should just consist of a series of actions that may throw
     // exceptions and should not have its own control flow.
+    if (session.user !== undefined) {
+      throw new UnauthenticatedError("Still logged in as " + session.user + "! Please log out before logging in.");
+    }
     session.user = username;
   }
 
